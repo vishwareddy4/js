11327bc0ec95a69dcd77d52db5e35ec7dcf7c665
vishwareddy4 <vishweshwarreddy44@gmail.com>
2023-07-11 12:22:59 -0500

    ticket price

--- first.js
+++ first.js
@@ -1,17 +1,19 @@
+var calculateTicketPrice = document.getElementById('submitButton');
+var outputPara = document.getElementById('ticketPrice');
 function calculateTicketPrice(age) {
-    let ticketPrice = 0;
-  
-    if (age < 0) {
-      return 'Invalid age';
-    } else if (age < 6) {
-      ticketPrice = 0;
-    } else if (age < 18) {
-      ticketPrice = 10;
-    } else if (age < 65) {
-      ticketPrice = 20;
-    } else {
-      ticketPrice = 15;
-    }
-  
-    return ticketPrice;
+  let ticketPrice = 0;
+
+  if (age < 0) {
+    return 'Invalid age';
+  } else if (age < 6) {
+    ticketPrice = 0;
+  } else if (age < 18) {
+    ticketPrice = 10;
+  } else if (age < 65) {
+    ticketPrice = 20;
+  } else {
+    ticketPrice = 15;
   }
+
+  return ticketPrice;
+}
